c07995d2ad50cf401192636846e988de74d2dc9a
Arijit panda <135040294+IpCode88@users.noreply.github.com>
2023-09-16 20:53:22 +0530

    added comment

--- src/main/java/com/cafe/example/cafeExample/dao/BillDao.java
+++ src/main/java/com/cafe/example/cafeExample/dao/BillDao.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.cafe.example.cafeExample.pojo.Bill;
-
+//Used to genetrate Bill Stuffs
 public interface BillDao extends JpaRepository<Bill, Integer> {
 	
 	List<Bill> getAllBillList();
